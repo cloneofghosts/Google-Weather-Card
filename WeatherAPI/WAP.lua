84e0c1e2686443b7a994aeeafeb65df553edc8da
Kev <thekpv8@gmail.com>
2021-04-24 14:34:20 -0400

    Add support for WeatherAPI.com
    - Visual Crossing skin now released

---
+++ WeatherAPI/WAP.lua
@@ -0,0 +1,29 @@
+function Initalize()
+end
+
+
+function GetDay(dayTime)
+	local Day =  os.date("%A", dayTime)
+	return Day
+end
+
+function GetTime(currTime)
+	local Hour, Minute, AMPM =  os.date("%I", currTime), os.date("%M", currTime), os.date("%p", currTime)
+	return Hour..':'..Minute.. ' ' ..AMPM
+end
+
+function GetIcon(CurrentCode, IsDay)
+	local icon
+	local daytime
+	if IsDay == 1 then
+		daytime = "_d"
+	else
+		daytime = "_n"
+	end
+	if CurrentCode == 1000 or CurrentCode == 1003 or CurrentCode == 1006 or CurrentCode == 1063 or CurrentCode == 1066 or CurrentCode == 1069 or CurrentCode == 1087 or CurrentCode == 1150 or CurrentCode == 1180 or CurrentCode == 1010 or CurrentCode == 1216 or CurrentCode == 1222 then
+		icon = CurrentCode .. daytime
+	else
+		icon = CurrentCode
+	end
+	return icon
+end
